00a195e99c7f02af8586102b9194c60a1ea03948
ginoliza <ginoangelolr@gmail.com>
2024-11-19 17:35:47 -0500

    vista /contact modificada

--- resources/views/contact.php
+++ resources/views/contact.php
@@ -3,8 +3,12 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>Brandon Contact</title>
 </head>
 <body>
+  <h1>Contacto</h1>
+  <p>Brandon Chavez</p>
+  <p>987654321</p>
+  <b>brad.chavez123@gmail.com</b>
 </body>
 </html>
